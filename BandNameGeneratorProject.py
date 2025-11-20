41309a7e758f86a84ddaacc10b1da316573ec79c
Mahad <128430576+Slattmoe@users.noreply.github.com>
2025-11-20 17:32:19 -0600

    Day1 Project

---
+++ BandNameGeneratorProject.py
@@ -0,0 +1,9 @@
+print("!!!! Hi welcome to the Band Name Generator Project !!!!\n")
+
+name = input("What's your name? \n")
+
+city = input("What city did you grow up in? \n")
+
+pet_name = input("What's the name of your pet? \n")
+
+print("Hi, " + name + " your band name would be: " + city + pet_name)
